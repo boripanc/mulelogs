22769189ac3aa699265bdd735b7b9170c687656f
boripanc <boripan.c@gmail.com>
2025-10-23 23:44:58 +0700

    #

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var format = require("date-format");
 const app = express()
 const port = 3000
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.post('/logs', async (req, res) => {
     console.log(req);
   var x = await db.none(
     "INSERT INTO public.analyticslogs "+
 "(apiid, apiversionid, orgid, hostid, clientid, transactionid, receivedts, repliedts, clientip, verb, \"path\", statuscode, useragent, requestbytes, responsebytes, requestdisposition, policyviolation, apiname, apiversion, instancename, applicationname, eventid, flowname, muleappname, deploymenttype) "+
-"VALUES(${apiId}, ${apiVersionId},${orgId},${hostId},${clientId},${transactionId},${receivedTs},${repliedTs},${clientIp},${verb},${path},${statusCode},${userAgent},${requestBytes},${responseBytes},${requestDisposition},${policyViolation},${apiName},${apiVersion},${instanceName},${applicationName},${eventId},${flowName},${muleAppName},${deploymentType});",req.body  );
+"VALUES ( ${apiId}, ${apiVersionId}, ${orgId}, ${hostId}, ${clientId}, ${transactionId}, ${receivedTs}, ${repliedTs}, ${clientIp}, ${verb}, ${path}, ${statusCode}, ${userAgent}, ${requestBytes}, ${responseBytes}, ${requestDisposition}, ${policyViolation}, ${apiName}, ${apiVersion}, ${instanceName}, ${applicationName}, ${eventId}, ${flowName}, ${muleAppName}, ${deploymentType});",req.body  );
   res.send({ status: "ok" });
 })
 
